d534339bcea01c22b39b934439198d0bad845404
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-09-03 14:52:47 +0300

    added even number checker

--- phase_3week_1.py
+++ phase_3week_1.py
@@ -1,5 +1,10 @@
 # Function to compute the sum of two numbers
-def sum_two_numbers(a, b):
-    return a + b
+def sum_two_numbers(num1, num2):
+    return num1 + num2
 print(sum_two_numbers(16, 29))
 
+# Function to determine if a number is Even/is divisible by 2
+def check_even(num):
+    return num % 2 == 0
+print(check_even(8))
+print(check_even(11))
