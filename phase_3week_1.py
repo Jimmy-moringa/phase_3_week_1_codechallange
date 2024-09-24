5aa06511e498c0836a9a61a2fbf4bbf0e58686e8
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-09-24 06:33:39 +0300

    done

--- phase_3week_1.py
+++ phase_3week_1.py
@@ -1,63 +1,132 @@
 # Function to compute the sum of two numbers
-def sum_two_numbers(num1, num2):
+def add_numbers(num1, num2):
     return num1 + num2
-print(sum_two_numbers(16, 29))
+print(add_numbers(16, 29))
+
+
+
+
+
+
 
 # Function to determine if a number is Even/is divisible by 2
-def check_even(num):
+def is_even(num):
     return num % 2 == 0
-print(check_even(8))
-print(check_even(11))
+print(is_even(8))
+print(is_even(11))
+
+
+
+
+
+
+
+
+
 
 # Function to reverse the given string first leter becomes the last one
 def reverse_string(s):
     return s[::-1]
 print(reverse_string("hello"))
 
+
+
+
+
+
+
+
+
+
 # Function to calculate the number of vowels in a string ie.my name
-def vowel_count(s):
+def count_vowels(s):
     vowels = "aeiou"
     return sum(char in vowels for char in s.lower())
-print(vowel_count("Jimmy Musyoki Mulundi"))
+print(count_vowels("Jimmy Musyoki Mulundi"))
 
 # method two
-def vowel_count(s):
+def count_vowels(s):
     vowels = "aeiou"
     return sum(char in vowels for char in s)
-print(vowel_count("Jimmy Musyoki Mulundi"))  
+print(count_vowels("Jimmy Musyoki Mulundi"))  
+
+
+
+
+
+
+
+
+
+
 
 # Function to compute the factorial of a given number
-def factorial_of(n):
+def calculate_factorial(n):
     if n == 0:
         return 1
     result = 1
     for i in range(1, n + 1):
         result *= i
     return result
-print(factorial_of(5))
+print(calculate_factorial(5))
 
 
 
-# Decorator function that applies a simple decoration to another function
-def simple_decorator(func):
+
+
+
+
+
+
+
+
+
+# Decorator function
+def decorator_func(func):
     def wrapper(*args, **kwargs):
+        print("Decorator Applied")
         return func(*args, **kwargs)
     return wrapper
 
-@simple_decorator
+# Function to apply the decorator
+def apply_decorator(func):
+    decorated_func = decorator_func(func)
+    return decorated_func
+
+# Example function to be decorated
 def say_hello(name):
     print(f"Hello, {name}!")
-say_hello("Jimmy")
+
+# Applying the decorator using the apply_decorator function
+decorated_say_hello = apply_decorator(say_hello)
+
+# Call the decorated function
+decorated_say_hello("Jimmy")
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Function to order a list of tuples based on age
-def order_by_age(people_list):
+def sort_by_age(people_list):
     return sorted(people_list, key=lambda person: person[1])
 people = [("jimmy", 74), ("mulundi", 55), ("musyoki", 27),("kalama", 24), ("erick", 35), ("degrace", 18)]
-print(order_by_age(people))
+print(sort_by_age(people))
+
+
+
 
 
 # Function to combine two dictionaries with value addition for common keys
-def combine_dicts(dict_a, dict_b):
+def merge_dicts(dict_a, dict_b):
     combined = dict_a.copy()
     for key, value in dict_b.items():
         if key in combined:
@@ -67,14 +136,17 @@ def combine_dicts(dict_a, dict_b):
     return combined
 dict_a = {'a': 9, 'b': 5}
 dict_b = {'b': 3, 'c': 7}
-print(combine_dicts(dict_a, dict_b))
+print(merge_dicts(dict_a, dict_b))
+
+
+
 
 
 
 
 
 # Class to represent a car with attributes and a method to display information
-class Vehicle:
+class car:
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
@@ -85,5 +157,5 @@ class Vehicle:
         print(f"Vehicle Model: {self.model}")
         print(f"Vehicle Year: {self.year}")
 
-my_vehicle = Vehicle("G-wagon", "Camry", 2024)
+my_vehicle = car("G-wagon", "Camry", 2024)
 my_vehicle.show_details()
